6703f069e45129f4e3ef5622a87089d891c5d658
kristinawithak <kristinawithak@users.noreply.github.com>
2019-01-22 14:16:26 -0500

    Add files via upload
    updated all files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+<script>
+	function openSlideMenu(){
+		document.getElementById('side-menu').style.width= '300px';
+	}
+		function closeSlideMenu(){
+		document.getElementById('side-menu').style.width= '0';
+	}
+	</script>
